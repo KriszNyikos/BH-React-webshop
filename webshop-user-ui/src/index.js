8d26697b747faa1bc30dae49baf85728a99af247
KriszNyikos <krisz.nyikos@gmail.com>
2020-05-08 21:51:28 +0200

    initial user-ui

--- webshop-user-ui/src/index.js
+++ webshop-user-ui/src/index.js
@@ -2,6 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux'
+import { createStore } from 'react-redux'
+
+const defaultState = { 
+  name: 'Teszt termék',
+  price: 23123,
+  quantity : 2,
+  items: [1, 2]
+}
+
+function reducer(state = defaultState, action) {
+
+  switch (action.type) {
+    case 'get':
+      return {
+        ...state
+      }
+  
+    default:
+     return state
+  }
+}
+
+const store = createStore(reducer) 
+
 
 ReactDOM.render(
   <Provider store={store}>
